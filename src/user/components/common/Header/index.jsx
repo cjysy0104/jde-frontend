af3b0f4f4ff36a04290ac98c312148b292eac2d3
Ahnjunyoung927 <younge64@naver.com>
2026-01-21 15:45:59 +0900

    feat: login

--- src/user/components/common/Header/index.jsx
+++ src/user/components/common/Header/index.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { FaUtensils } from 'react-icons/fa';
 import Logo1 from '../../../../assets/logo.png';
+import { AuthContext } from '../../context/AuthContext';
 
 import {
   HeaderContainer,
@@ -13,25 +13,45 @@ import {
   AuthButton,
 } from './styles';
 
+
 const Header = () => {
   const navigate = useNavigate();
+
+  // 인증 상태 가져오기
+  const { auth, logout } = useContext(AuthContext);
 
   return (
     <HeaderContainer>
       <HeaderContent>
-        <LogoSection>
+        <LogoSection onClick={() => navigate('/')}>
           <LogoImage src={Logo1} alt="JUST DO EAT" />
           <LogoText>JUST DO EAT</LogoText>
         </LogoSection>
 
-
         <AuthButtons>
-          <AuthButton onClick={() => navigate('/login')}>
-            로그인
-          </AuthButton>
-          <AuthButton>
-            회원가입
-          </AuthButton>
+          {/* 비로그인 상태 */}
+          {!auth.isAuthenticated && (
+            <>
+              <AuthButton onClick={() => navigate('/login')}>
+                로그인
+              </AuthButton>
+              <AuthButton onClick={() => navigate('/signup')}>
+                회원가입
+              </AuthButton>
+            </>
+          )}
+
+          {/* 로그인 상태 */}
+          {auth.isAuthenticated && (
+            <>
+              <AuthButton onClick={() => navigate('/mypage')}>
+                마이페이지
+              </AuthButton>
+              <AuthButton onClick={logout}>
+                로그아웃
+              </AuthButton>
+            </>
+          )}
         </AuthButtons>
       </HeaderContent>
     </HeaderContainer>

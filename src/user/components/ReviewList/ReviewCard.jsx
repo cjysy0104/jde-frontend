85f406a4619bd9f83459ddbe43f7e4b4f8889cd7
yoosh0610 <yoo31318@gmail.com>
2026-01-26 16:38:04 +0900

    feat:리뷰좋아요 기능, feat:북마크 추가/취소 기능

--- src/user/components/ReviewList/ReviewCard.jsx
+++ src/user/components/ReviewList/ReviewCard.jsx
@@ -24,7 +24,7 @@ import {
 } from './ReviewCard.styled';
 
 const ReviewCard = ({ review, onLike, onBookmark }) => {
-  const liked = !!review.uiLiked;
+  const liked = review.uiLiked;
   const marked = review.isMarked === "Y";
   return (
     <Card>

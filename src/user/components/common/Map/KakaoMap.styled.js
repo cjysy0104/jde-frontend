4e3186899ffd78e8176b40cfae74db65f7b10e16
yoosh0610 <yoo31318@gmail.com>
2026-01-30 09:17:06 +0900

    feat:신고기능

--- src/user/components/common/Map/KakaoMap.styled.js
+++ src/user/components/common/Map/KakaoMap.styled.js
@@ -4,7 +4,7 @@ export const MapWrapper = styled.div`
   width: 100%;
   height: 100%;
   min-height: 400px;
-  position: absolute;
+  position: relative;
   inset: 0;
   border-radius: 12px;
   overflow: hidden;
